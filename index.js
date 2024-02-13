bbc59ce7f26ccbdda27ee743aa2e7e398cdea1ae
Johan Griesel <johan@stratusolve.com>
2024-02-13 10:14:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export const defaultFunction = () => {
-    console.log("Default function called with no arguments");
+    console.log("Default function called with no arguments. divblox cli version 0.0.1");
 };
 
 export const doSomething = () => {
