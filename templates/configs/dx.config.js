8eb0922562abdf76a46a734ec5622edb70e27e38
Dani <dani.simeonov@stratusolve.com>
2024-06-28 13:28:39 +0200

    Update dx.config.js

--- templates/configs/dx.config.js
+++ templates/configs/dx.config.js
@@ -1,18 +1,25 @@
 // Every property can be overwritten by node ENV variables of the same name
 export default {
     webFramework: "sveltekit", // Supported web frameworks ['none'|'sveltekit']
-    dxApiKey: undefined, // Divblox API key used to access resources stored there
     environment: "local", // Current environment,
     dataModelPath: "divblox/configs/datamodel.json", // Path from route to the data model JSON file
     databaseConfigPath: "divblox/configs/database.config.js", // Path from root to the database configuration file
     databaseCaseImplementation: "snakecase", // Allowed options ['snakecase'|'camelcase'|'pascalcase']
-    ormImplementation: "prisma", // Supported options ['none','prisma']; If defined, the relevant orm will automatically be installed and used for things like component generation
+    /** Supported options ['none','prisma'];
+        If defined, the relevant orm will automatically be
+        installed and used for things like component generation 
+    */
+    ormImplementation: "prisma",
     codeGen: {
-        // Configurations for where and how to generate data model code
+        uiImplementation: "[uiImplementation]", // Supported options: ['none', 'tailwindcss', 'shadcn']
         dataModelUiConfigPath: "divblox/code-gen/datamodel-ui.config.json", // Path from route to the data model UI configuration file
-        // componentsPath: { fromRoot: "/src/lib/dx-components", alias: "$lib/dx-components" },
-        componentsPath: { fromRoot: "[componentsPathFromRoot]", alias: "[componentsPathAlias]" },
-        routesPath: { fromRoot: "/src/routes", alias: "$src/routes" },
-        uiImplementation: "[uiImplementation]",
+        componentsPath: {
+            fromRoot: "[componentsPathFromRoot]", // Path to the folder to generate data-model components in
+            alias: "[componentsPathAlias]", // Aliased path to the folder to generate data-model components in
+        },
+        routesPath: {
+            fromRoot: "/src/routes", // Path to the folder to generate routes in
+            alias: "$src/routes", // Aliased path to the folder to generate routes in
+        },
     },
 };
