8aa7ad39bce3e67ae57714540245d30cdc98380b
Dani <dani.simeonov@stratusolve.com>
2024-06-28 13:34:43 +0200

    Update dx.config.js

--- templates/configs/dx.config.js
+++ templates/configs/dx.config.js
@@ -1,6 +1,7 @@
 // Every property can be overwritten by node ENV variables of the same name
 export default {
     webFramework: "sveltekit", // Supported web frameworks ['none'|'sveltekit']
+    dxApiKey: undefined, // Divblox API key used to access resources stored there
     environment: "local", // Current environment,
     dataModelPath: "divblox/configs/datamodel.json", // Path from route to the data model JSON file
     databaseConfigPath: "divblox/configs/database.config.js", // Path from root to the database configuration file
